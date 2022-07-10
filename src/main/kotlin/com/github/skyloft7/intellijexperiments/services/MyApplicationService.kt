1cb06e6f8d8037ed1752604693de5a88c3570807
GitHub Action <action@github.com>
2022-07-10 10:45:14 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/skyloft7/intellijexperiments/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.skyloft7.intellijexperiments.services
+
+import com.github.skyloft7.intellijexperiments.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
